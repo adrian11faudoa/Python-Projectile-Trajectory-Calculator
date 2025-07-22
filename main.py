149086eaa1e07905833731a4f42711b37e4b2453
Adrian11Faudoa <afa1823@gmail.com>
2025-07-21 22:52:38 -0600

    Update Code

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ class Projectile:
     def __str__(self):
         return f'''
 Projectile details:
-speed: {self.__speed} m/s
-height: {self.__height} m
-angle: {round(math.degrees(self.__angle))}°
+speed: {self.speed} m/s
+height: {self.height} m
+angle: {self.angle}°
 displacement: {round(self.__calculate_displacement(), 1)} m
 '''
 
@@ -46,8 +46,76 @@ displacement: {round(self.__calculate_displacement(), 1)} m
             for x in range(math.ceil(self.__calculate_displacement()))
         ]
 
-    
+    @property
+    def height(self):
+        return self.__height
 
-ball = Projectile(10, 3, 45)
-print(ball)
-coordinates = ball.calculate_all_coordinates()
+    @property
+    def angle(self):
+        return round(math.degrees(self.__angle))
+
+    @property
+    def speed(self):
+        return self.__speed
+
+    @height.setter
+    def height(self, n):
+        self.__height = n
+
+    @angle.setter
+    def angle(self, n):
+        self.__angle = math.radians(n)
+
+    @speed.setter
+    def speed(self, s):
+       self.__speed = s
+    
+    def __repr__(self):
+        return f'{self.__class__}({self.speed}, {self.height}, {self.angle})'
+
+class Graph:
+    __slots__ = ('__coordinates')
+
+    def __init__(self, coord):
+        self.__coordinates = coord
+
+    def __repr__(self):
+        return f"Graph({self.__coordinates})"
+
+    def create_coordinates_table(self):
+        table = '\n  x      y\n'
+        for x, y in self.__coordinates:
+            table += f'{x:>3}{y:>7.2f}\n'
+
+        return table
+
+    def create_trajectory(self):
+
+        rounded_coords = [(round(x), round(y)) for x, y in self.__coordinates]
+
+        x_max = max(rounded_coords, key=lambda i: i[0])[0]
+        y_max = max(rounded_coords, key=lambda j: j[1])[1]
+
+        matrix_list = [[" " for _ in range(x_max + 1)] for _ in range(y_max + 1)]
+
+        for x, y in rounded_coords:
+            matrix_list[-1 - y][x] = PROJECTILE
+
+        matrix = ["".join(line) for line in matrix_list]
+
+        matrix_axes = [y_axis_tick + row for row in matrix]
+        matrix_axes.append(" " + x_axis_tick * (len(matrix[0])))
+
+        graph = "\n" + "\n".join(matrix_axes) + "\n"
+
+        return graph
+
+def projectile_helper(speed, height, angle):
+    ball = Projectile(speed, height, angle)
+    print(ball)
+    coordinates = ball.calculate_all_coordinates()
+    graph = Graph(coordinates)
+    print(graph.create_coordinates_table())
+    print(graph.create_trajectory())
+
+projectile_helper(15, 3, 45)
